ecf5aa926e6a9b4535d0b16231a8a445d2f499d0
Gautham Suresh <gautham.suresh@integris-education.com>
2024-11-21 17:46:27 +0530

    create blog page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
+use App\Form\BlogType;
+use App\Repository\BlogPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(BlogPostRepository $blogs): Response
     {
         return $this->render('blog/index.html.twig', [
-            'message' => 'Blog Landing Page',
+            'blogs' => $blogs->findAll()
         ]);
+    }
+
+    #[Route('/createblog', name: 'app_blog_create')]
+    public function store(Request $request, EntityManagerInterface $manager): Response 
+    {
+        $form = $this->createForm(BlogType::class, new BlogPost );
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $posts = $form->getData();
+
+            $manager->persist($posts, true);
+            $manager->flush();
+
+            $this->addFlash('success', 'New Blog Created Successfully');
+
+            return $this->redirectToRoute('app_blog');
+        }
+
+        return $this->render(
+            'blog/createBlog.html.twig',
+            [
+                'form' => $form
+            ]
+        );
     }
 }
